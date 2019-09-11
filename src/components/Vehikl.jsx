9ec8eff16ccfaca1a95d65ea4ce3852e4f2cb9fa
Jarod Burchill <jarodburchill@gmail.com>
2019-09-11 11:19:32 -0400

    Added svgs

--- src/components/Vehikl.jsx
+++ src/components/Vehikl.jsx
@@ -1,10 +1,21 @@
 import React from "react";
 import car from "../assets/car.svg";
+import compact from "../assets/compact.svg";
+import jeep from "../assets/jeep.svg";
+import bus from "../assets/bus.svg";
+import truck from "../assets/truck.svg";
+import suv from "../assets/suv.svg";
 
 function Vehikl() {
+  const getVehikl = () => {
+    const vehikls = [car, compact, jeep, bus, truck, suv];
+    const random = Math.floor(Math.random() * 6);
+    return vehikls[random];
+  };
+
   return (
     <div>
-      <img src={car} alt="Car" />
+      <img src={getVehikl()} alt="vehikl" />
     </div>
   );
 }
