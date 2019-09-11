d60f06bda9e91dab5fc6c0e03215e1dad5440cae
Jarod Burchill <jarodburchill@gmail.com>
2019-09-11 11:00:00 -0400

    Spot hover

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./styles/global.css";
+import "./styles/global.scss";
 import App from "./components/App";
 
 ReactDOM.render(<App />, document.getElementById("root"));
